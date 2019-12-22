e2499d3a3532d409382430129d4d8f80b91665cc
Edward Minnix III <egregius313@gmail.com>
2019-12-21 22:26:20 -0500

    Initial commit

---
+++ lobsters/rss.go
@@ -0,0 +1,72 @@
+package lobsters
+
+import (
+	"encoding/xml"
+	"io"
+	"time"
+)
+
+const PubDateFormat = "Mon, 2 Jan 2006 15:04:05 -0700"
+
+type Guid struct {
+	XMLName     xml.Name `xml:"guid"`
+	IsPermalink bool     `xml:"isPermalink,attr"`
+	Id          string   `xml:",chardata"`
+}
+
+type Item struct {
+	XMLName     xml.Name  `xml:"item"`
+	Title       string    `xml:"title"`
+	Link        string    `xml:"link"`
+	Guid        Guid      `xml:"guid"`
+	Author      string    `xml:"author"`
+	PubDate     PubDate   `xml:"pubDate"`
+	Comments    string    `xml:"comments"`
+	Description string    `xml:"description"`
+	Categories  []string  `xml:"category"`
+}
+
+type PubDate time.Time
+
+func (pd *PubDate) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var v string
+	d.DecodeElement(&v, &start)
+    parse, err := time.Parse(PubDateFormat, v)
+    if err != nil {
+        return err
+    }
+	*pd = PubDate(parse)
+	return err
+}
+
+type Channel struct {
+	XMLName     xml.Name `xml:"channel"`
+	Description string   `xml:"description"`
+	Link        string   `xml:"link"`
+	AtomLink    AtomLink `xml:"atom:link"`
+	Items       []Item   `xml:"item"`
+}
+
+type AtomLink struct {
+	XMLName   xml.Name `xml:"atom:link"`
+	Reference string   `xml:"href,attr"`
+	Relation  string   `xml:"rel,attr"`
+	Type      string   `xml:"type,attr"`
+}
+
+type Rss struct {
+	XMLName   xml.Name `xml:"rss"`
+	Version   string   `xml:"version,attr"`
+	Namespace string   `xml:"xmlns:atom,attr"`
+	Channel   Channel  `xml:"channel"`
+}
+
+func DecodeItems(reader io.Reader) ([]Item, error) {
+	decoder := xml.NewDecoder(reader)
+	feed := Rss{}
+	err := decoder.Decode(&feed)
+	if err != nil {
+		return nil, err
+	}
+	return feed.Channel.Items, nil
+}
---
+++ lobsters/rss_test.go
@@ -0,0 +1,54 @@
+package lobsters
+
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestItem(t *testing.T) {
+	blob := `
+      <item>
+        <title>A Beginner&#39;s Look at BenchmarkTools.jl</title>
+        <link>https://randyzwitch.com/benchmarktools-julia-benchmarking/</link>
+        <guid isPermaLink="false">https://lobste.rs/s/m9zan1</guid>
+        <author>jstuartmill@users.lobste.rs (jstuartmill)</author>
+        <pubDate>Fri, 20 Dec 2019 21:51:22 -0600</pubDate>
+        <comments>https://lobste.rs/s/m9zan1/beginner_s_look_at_benchmarktools_jl</comments>
+        <description>
+          
+            &#60;p&#62;&#60;a href=&#34;https://lobste.rs/s/m9zan1/beginner_s_look_at_benchmarktools_jl&#34;&#62;Comments&#60;/a&#62;&#60;/p&#62;
+        </description>
+          <category>programming</category>
+      </item>`
+
+	got := Item{}
+	err := xml.Unmarshal([]byte(blob), &got)
+	if err != nil {
+		t.Errorf("%q", err)
+	}
+	fmt.Println(got)
+	fmt.Println(time.Time(got.PubDate))
+
+	if len(got.Categories) != 1 {
+		t.Errorf("len(Categories) = %d; want 1", len(got.Categories))
+	}
+}
+
+func TestRss(t *testing.T) {
+	file, err := os.Open("test/programming.rss")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	decoder := xml.NewDecoder(file)
+	rss := Rss{}
+	decoder.Decode(&rss)
+	//fmt.Println(rss)
+	if len(rss.Channel.Items) == 0 {
+		fmt.Println("oops")
+	}
+}
